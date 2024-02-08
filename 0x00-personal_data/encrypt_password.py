7bd713f22d1a4614f09f95bbe430c9ec604a62d5
Fadyy22 <fadyalaa441@gmail.com>
2024-02-08 18:18:19 +0200

    add is_valid func to check passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,9 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """returns salted and hashed password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks if the provided password matches the hashed password"""
+    correct = bcrypt.checkpw(password.encode(), hashed_password)
+    return correct
