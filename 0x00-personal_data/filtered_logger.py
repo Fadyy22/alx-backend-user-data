97b47092335a68019957cbd67007d567870e4868
Fadyy22 <fadyalaa441@gmail.com>
2024-02-08 17:24:56 +0200

    implement format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 """module for all tasks from 0 to 5"""
-from typing import List
+import logging
 import re
+from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
 
 
 def filter_datum(fields: List[str],
