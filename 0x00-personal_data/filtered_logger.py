dbf222771f728e20d0f33bef53246bcb722a8cf8
Fadyy22 <fadyalaa441@gmail.com>
2024-02-08 17:26:38 +0200

    implement format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,6 +18,7 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """returns formatted log record"""
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.msg, self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
