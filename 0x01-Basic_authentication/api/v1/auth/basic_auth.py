38c8371486dc51f34681b26f83f8328fe3bd60dc
Fadyy22 <fadyalaa441@gmail.com>
2024-02-13 01:00:03 +0200

    implement method to get user based on email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,9 +43,8 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """returns the User instance based on his email and password"""
-        if (not user_email or not isinstance(user_email, str)
-                or not user_pwd or not isinstance(user_pwd, str)):
-            return None
-
-        user = User.search({"email": user_email})
-        print(user)
+        if isinstance(user_email, str) and isinstance(user_pwd, str):
+            user = User.search({"email": user_email})
+            if user and user[0].is_valid_password(user_pwd):
+                return user[0]
+        return None
