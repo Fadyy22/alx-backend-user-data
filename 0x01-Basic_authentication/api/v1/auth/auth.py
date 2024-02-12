6e81af69986396050d966bad8d7bcbb0b008a3b5
Fadyy22 <fadyalaa441@gmail.com>
2024-02-12 20:56:57 +0200

    create template class for auth system

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""module for Auth class that handles authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class for handling authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """returns None"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None"""
+        return None
