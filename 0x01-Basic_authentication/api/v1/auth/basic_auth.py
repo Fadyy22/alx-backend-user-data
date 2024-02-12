c1d2c4cd18d6d4ace78af44b79f4e038aa01c21e
Fadyy22 <fadyalaa441@gmail.com>
2024-02-13 01:29:20 +0200

    implement method to get the current user from auth header basic token

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -51,3 +51,12 @@ class BasicAuth(Auth):
             except Exception:
                 return None
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """retrieves the User instance for a request"""
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        user = self.user_object_from_credentials(email, password)
+        return user
