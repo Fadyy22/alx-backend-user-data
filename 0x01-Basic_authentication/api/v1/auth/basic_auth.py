632005dbbd99c13a69a26d52f558104491766f42
Fadyy22 <fadyalaa441@gmail.com>
2024-02-13 00:41:42 +0200

    implement method to extract basic auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ from flask import request
 from base64 import b64decode
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -13,11 +14,10 @@ class BasicAuth(Auth):
                                             authorization_header: str) -> str:
         """returns the Base64 part
         of the Authorization header for a Basic Authentication"""
-        if (not authorization_header
-                or not isinstance(authorization_header, str)
-                or not authorization_header.startswith("Basic ")):
-            return None
-        return authorization_header.split()[1]
+        if isinstance(authorization_header, str):
+            if authorization_header.startswith("Basic "):
+                return authorization_header.split()[1]
+        return None
 
     def decode_base64_authorization_header(
             self,
@@ -43,3 +43,13 @@ class BasicAuth(Auth):
                 or ":" not in decoded_base64_authorization_header):
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on his email and password"""
+        if (not user_email or not isinstance(user_email, str)
+                or not user_pwd or not isinstance(user_pwd, str)):
+            return None
+
+        user = User.search({"email": user_email})
+        print(user)
