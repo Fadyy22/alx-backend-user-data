f9d70d265215fa2b63e63d22bfaa8a101d13ec54
Fadyy22 <fadyalaa441@gmail.com>
2024-02-16 01:37:07 +0200

    add class for session auth mechanism

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,6 +7,9 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -14,12 +17,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if getenv("AUTH_TYPE") == "auth":
-    from api.v1.auth.auth import Auth
+auth_type = getenv("AUTH_TYPE")
+if auth_type == "auth":
     auth = Auth()
-elif getenv("AUTH_TYPE") == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
+elif auth_type == "basic_auth":
     auth = BasicAuth()
+elif auth_type == "session_auth":
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module for SessionAuth class that handles
+session authentication mechanism"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """SessionAuth class for handling session authentication mechanism"""
+    pass
