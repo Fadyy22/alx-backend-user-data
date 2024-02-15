26c9d60c08293f6560b4382f9bd6f80bb41ae123
Fadyy22 <fadyalaa441@gmail.com>
2024-02-16 01:48:21 +0200

    add create_session method to create and store in memory session_id for user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 """module for SessionAuth class that handles
 session authentication mechanism"""
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """SessionAuth class for handling session authentication mechanism"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if isinstance(user_id, str):
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
