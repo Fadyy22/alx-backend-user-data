a503ebc580f466db940e363c294d6c7ca725a20e
Fadyy22 <fadyalaa441@gmail.com>
2024-02-16 14:01:58 +0200

    remove imports from auth and basic_auth

--- 0x02-Session_authentication/api/v1/auth/basic_auth.py
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """module for BasicAuth class that handles authentication"""
-from flask import request
 from base64 import b64decode
-from typing import List, TypeVar
+from typing import TypeVar
 from api.v1.auth.auth import Auth
 from models.user import User
 
