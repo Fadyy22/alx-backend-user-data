dd35de2ae8a08d896bb5102040de49df45badb8f
Fadyy22 <fadyalaa441@gmail.com>
2024-02-22 14:57:50 +0200

    add PUT /reset_password to update user's password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -23,7 +23,7 @@ def users():
         email = request.form.get("email")
         password = request.form.get("password")
         AUTH.register_user(email, password)
-        return jsonify({"email": f"{email}", "message": "user created"})
+        return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
@@ -36,7 +36,7 @@ def login():
     user = AUTH.valid_login(email, password)
     if user:
         session_id = AUTH.create_session(email)
-        res = jsonify({"email": f"{email}", "message": "logged in"})
+        res = jsonify({"email": email, "message": "logged in"})
         res.set_cookie("session_id", session_id)
         return res
     abort(401)
@@ -59,7 +59,7 @@ def profile():
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        return jsonify({"email": f"{user.email}"}), 200
+        return jsonify({"email": user.email}), 200
     abort(403)
 
 
@@ -69,8 +69,21 @@ def get_reset_password_token():
     email = request.form.get("email")
     try:
         reset_token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": f"{email}",
-                        "reset_token": f"{reset_token}"}), 200
+        return jsonify({"email": email,
+                        "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """updates user password"""
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
     except ValueError:
         abort(403)
 
