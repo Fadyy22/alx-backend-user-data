dd7bdab8de917cba8dc2e460c4e8be0129e2f5a2
Fadyy22 <fadyalaa441@gmail.com>
2024-02-22 14:43:34 +0200

    add update_password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,6 +78,16 @@ class Auth:
         except InvalidRequestError or NoResultFound:
             raise ValueError
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """updates the user's password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password).decode("utf-8")
+            self._db.update_user(user.id, hashed_password=hashed_password)
+            return None
+        except InvalidRequestError or NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """method that takes in a password string arguments and returns bytes
